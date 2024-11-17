84ce19d494b0e604d869ed2864b6f4c47197867a
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2024-11-17 16:42:20 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+def main():
+    print("hello world!")
+    name = input("What is your name? ")
+    print("Nice to meet you,", name)
+
+if __name__ == "__main__":
+    main()
