805aa54eac1f049f7dba7007f9fe8d74423a144c
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2024-11-17 16:37:23 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,15 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+# import the math module, which contains features for working with mathematics
+import math
+
+# the math module contains lots of pre-built functions
+print("The square root of 16 is", math.sqrt(16))
+
+# in addition to functions, some modules contain useful constants 
+print("Pi is:", math.pi)
